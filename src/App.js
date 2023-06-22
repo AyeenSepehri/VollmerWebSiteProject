70f29715e76460cd8f4d1f84272a342a38999d9e
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2023-06-22 19:30:54 +0330

    doing some test in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Banner from './components/BannerFolder/Banner';
+import textBox from "./assets/textBox/finalBox2.png"
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <h1>helllllo world</h1>
+      <p>lkdhfashdfkjasdfhakjsdhfkasjdlfjahsd.ifha/lsdkfih/dh/fashdfahsd.fkjshdkfjah/skdjhf.audfhyaiua</p>
+      <img src={textBox}/>
     </div>
   );
 }
